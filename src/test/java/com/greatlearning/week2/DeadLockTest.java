deea088a3087b78d5ec740a10b7b80e77d7f3562
Manoj Kumar <manoj.er.kumar@gmail.com>
2022-01-08 08:21:05 +0530

    Added the example of Deadlock, LockGraphFacade,DeadlockTimeout

---
+++ src/test/java/com/greatlearning/week2/DeadLockTest.java
@@ -0,0 +1,14 @@
+package com.greatlearning.week2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeadLockTest {
+
+    @Test
+    public void first(){
+
+    }
+
+}
